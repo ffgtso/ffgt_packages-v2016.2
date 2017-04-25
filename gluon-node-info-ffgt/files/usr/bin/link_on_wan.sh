a5b707c13cb4f7ec3021a2b37e4936ee080bc6d4
Kai Siering <k.siering@team.mail.de>
2017-04-25 18:10:52 +0200

    Prevent races

--- gluon-node-info-ffgt/files/usr/bin/link_on_wan.sh
+++ gluon-node-info-ffgt/files/usr/bin/link_on_wan.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat /sys/class/net/$(lua -e 'print(require("gluon.sysconfig").wan_ifname)')/carrier >/tmp/link_on_wan
+cat /sys/class/net/$(lua -e 'print(require("gluon.sysconfig").wan_ifname)')/carrier >/tmp/link_on_wan.tmp && mv /tmp/link_on_wan.tmp /tmp/link_on_wan
